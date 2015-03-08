e1d428d5f66e712c4001f1578e36eccf00f53aa7
pedrofsn <pedrokra@gmail.com>
2015-03-08 20:12:43 -0300

    Remoção de comentários desnecessários

--- app/src/main/java/br/com/gwaya/jopy/App.java
+++ app/src/main/java/br/com/gwaya/jopy/App.java
@@ -15,14 +15,10 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
-        // Initialize the singletons so their instances
-        // are bound to the application process.
         initSingletons();
     }
 
     protected void initSingletons() {
-        // Initialize the instance of MySingleton
         DatabaseManager.initializeInstance(new MySQLiteHelper(getApplicationContext()));
     }
 

b9e447b20527f3ba19a9da78f34856d8446323f9
Gwaya da Brasil Ltda <diego@gwaya.com>
2015-03-20 16:37:05 -0300

    Executa task para informar a API que o usuário fez logout

--- app/src/main/java/br/com/gwaya/jopy/activity/abas/ActivitySobre.java
+++ app/src/main/java/br/com/gwaya/jopy/activity/abas/ActivitySobre.java
@@ -8,11 +8,16 @@ import android.view.View;
 import android.webkit.WebView;
 import android.widget.TextView;
 
+import java.util.List;
+
 import br.com.gwaya.jopy.R;
 import br.com.gwaya.jopy.activity.ActivityLogin;
 import br.com.gwaya.jopy.activity.abstracoes.Aba;
 import br.com.gwaya.jopy.dao.AcessoDAO;
 import br.com.gwaya.jopy.dao.PedidoCompraDAO;
+import br.com.gwaya.jopy.model.Acesso;
+import br.com.gwaya.jopy.tasks.LogoutAsyncTask;
+
 
 /**
  * Created by marcelorosa on 11/01/15.
@@ -34,6 +39,18 @@ public class ActivitySobre extends Aba {
             @Override
             public void onClick(View v) {
                 try {
+
+                    // Diego - 20/30 - informa para API com intuito de parar as push notifications para o device
+                    List<Acesso> lstAcesso = new AcessoDAO().getAllAcesso();
+
+                    if (lstAcesso.size() > 0) {
+
+                        Acesso acesso = lstAcesso.get(0);
+
+                        LogoutAsyncTask logout = new LogoutAsyncTask(ActivitySobre.this.getApplicationContext(), acesso);
+                        logout.execute();
+
+                    }
 
                     new AcessoDAO().deleteAcesso();
                     new PedidoCompraDAO().deleteAll();

bfc3427f33cf733f1b7e5364a9b39e8e9f136691
pedrofsn <pedrokra@gmail.com>
2015-03-22 10:49:00 -0300

    Refactoring

--- app/src/main/java/br/com/gwaya/jopy/communication/PedidoCompraReceiver.java
+++ app/src/main/java/br/com/gwaya/jopy/communication/PedidoCompraReceiver.java
@@ -4,7 +4,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-
 public class PedidoCompraReceiver extends BroadcastReceiver {
 
     @Override

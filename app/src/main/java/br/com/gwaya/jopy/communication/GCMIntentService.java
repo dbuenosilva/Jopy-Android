1cde5c18ccdefd54330e00cc8a7ed4e33f0e4c84
Gwaya da Brasil Ltda <diego@gwaya.com>
2015-03-24 13:56:39 -0300

    Correcoes do Push Notification

--- app/src/main/java/br/com/gwaya/jopy/communication/GCMIntentService.java
+++ app/src/main/java/br/com/gwaya/jopy/communication/GCMIntentService.java
@@ -1,10 +1,13 @@
 package br.com.gwaya.jopy.communication;
 
 import android.app.IntentService;
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.v4.app.NotificationCompat;
@@ -14,7 +17,9 @@ import com.google.android.gms.gcm.GoogleCloudMessaging;
 
 import br.com.gwaya.jopy.App;
 import br.com.gwaya.jopy.R;
-import br.com.gwaya.jopy.activity.ActivityLogin;
+import br.com.gwaya.jopy.activity.ActivityMain;
+
+import static br.com.gwaya.jopy.utils.CommonUtilities.displayMessage;
 
 public class GCMIntentService extends IntentService {
 
@@ -60,8 +65,11 @@ public class GCMIntentService extends IntentService {
                 }
                 Log.i(App.TAG, "Completed work @ " + SystemClock.elapsedRealtime());
                 // Post notification of received message.
-                sendNotification("Received: " + extras.toString());
-                Log.i(App.TAG, "Received: " + extras.toString());
+                sendNotification(extras.get("message").toString());
+                Log.i(App.TAG, "Received: " + extras.get("message").toString());
+                displayMessage(this.getApplicationContext(), extras.get("message").toString());
+
+
             }
         }
         // Release the wake lock provided by the WakefulBroadcastReceiver.
@@ -75,18 +83,49 @@ public class GCMIntentService extends IntentService {
         NotificationManager mNotificationManager = (NotificationManager)
                 this.getSystemService(Context.NOTIFICATION_SERVICE);
 
-        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
-                new Intent(this, ActivityLogin.class), 0);
+        App.ABA_ATUAL = 0; // Seta aba Pendentes
+
+        Intent intent = new Intent(this, ActivityMain.class);
+        intent.putExtra("msg", msg);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
+                intent, 0);
+
+        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        if(alarmSound == null){
+            alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+            if(alarmSound == null){
+                alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+            }
+        }
 
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(this)
                         .setSmallIcon(R.drawable.ic_launcher)
                         .setContentTitle(getString(R.string.app_name))
+                        .setDefaults(Notification.DEFAULT_ALL)
+                        .setSound(alarmSound)
+                        .setDefaults(Notification.DEFAULT_VIBRATE)
+                        .setAutoCancel(true)
                         .setStyle(new NotificationCompat.BigTextStyle()
                                 .bigText(msg))
                         .setContentText(msg);
 
-        mBuilder.setContentIntent(contentIntent);
+        // DIEGO - Tentei criando o metodo getDialogPendingIntent mas também não deu certo...
+        mBuilder.setContentIntent( getDialogPendingIntent(msg, intent.toString()) );
         mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+    }
+
+
+    private PendingIntent getDialogPendingIntent(String dialogText,
+                                                 String intentname) {
+        return PendingIntent.getActivity(
+                this,
+                dialogText.hashCode(),
+                new Intent()
+                        .putExtra(Intent.EXTRA_TEXT, dialogText)
+                        .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                        .setAction(intentname), 0);
+
     }
 }

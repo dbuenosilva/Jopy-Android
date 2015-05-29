4cce8f84b1df620331e9b5345ac039173f3b0eca
pedrofsn <pedrokra@gmail.com>
2015-05-28 21:20:49 -0300

    Validando permissões de acesso (não os níveis) na tela de menu

--- app/src/main/java/br/com/gwaya/jopy/activity/ActivityMenu.java
+++ app/src/main/java/br/com/gwaya/jopy/activity/ActivityMenu.java
@@ -6,7 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import java.util.List;
+
 import br.com.gwaya.jopy.R;
+import br.com.gwaya.jopy.controller.ControllerPermissao;
+import br.com.gwaya.jopy.enums.Acesso;
+import br.com.gwaya.jopy.model.Permissao;
 
 /**
  * Created by pedrofsn on 23/05/15.
@@ -14,18 +19,19 @@ import br.com.gwaya.jopy.R;
 public class ActivityMenu extends Activity implements View.OnClickListener {
 
     private boolean login;
+    private List<Permissao> listaPermissoes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        Bundle extras = getIntent().getExtras();
-        login = extras.getBoolean("login", true);
+        ControllerPermissao controllerPermissao = new ControllerPermissao();
+        listaPermissoes = controllerPermissao.readAll();
 
         findViewById(R.id.viewCompras).setOnClickListener(this);
-        findViewById(R.id.viewOrcamentos).setOnClickListener(this);
         findViewById(R.id.viewVendas).setOnClickListener(this);
+        findViewById(R.id.viewOrcamentos).setOnClickListener(this);
         findViewById(R.id.viewContasPagar).setOnClickListener(this);
     }
 
@@ -33,15 +39,35 @@ public class ActivityMenu extends Activity implements View.OnClickListener {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.viewCompras:
-                Intent intent = new Intent(this, ActivityMain.class);
-                intent.putExtra("login", login);
-                startActivity(intent);
+                if (validarPermissao(Acesso.COMPRAS)) {
+                    Intent intent = new Intent(this, ActivityMain.class);
+                    intent.putExtra("login", login);
+                    startActivity(intent);
+                }
+                break;
+            case R.id.viewVendas:
+                if (validarPermissao(Acesso.VENDAS)) {
+
+                }
                 break;
             case R.id.viewOrcamentos:
-            case R.id.viewVendas:
+                if (validarPermissao(Acesso.ORCAMENTO)) {
+
+                }
+                break;
             case R.id.viewContasPagar:
-                Toast.makeText(this, getString(R.string.funcionalidade_em_desenvolvimento), Toast.LENGTH_SHORT).show();
+                if (validarPermissao(Acesso.FINANCEIRO)) {
+
+                }
                 break;
         }
+    }
+
+    private boolean validarPermissao(Acesso acesso) {
+        for (Permissao permissao : listaPermissoes) {
+            return acesso.getValor() == permissao.getAcesso();
+        }
+        Toast.makeText(this, getString(R.string.solucao_em_desenvolvimento), Toast.LENGTH_SHORT).show();
+        return false;
     }
 }
